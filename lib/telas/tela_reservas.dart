790f3769f3f4bf260c907e4902fc7432bb811a2f
Alan Willian Melo <amelo967@gmail.com>
2019-10-08 17:19:29 -0300

    Alan

--- lib/telas/tela_reservas.dart
+++ lib/telas/tela_reservas.dart
@@ -1,9 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:shareon/item_listas/lista_reservas.dart';
 import 'package:shareon/layout_listas/lista_reservas_builder.dart';
-import 'package:shareon/utils/drawer_builder.dart';
-import 'package:shareon/utils/shareon_appbar.dart';
 import 'package:shareon/utils/texts.dart';
 
 class TelaReservas extends StatefulWidget {
@@ -15,15 +12,13 @@ class _TelaReservasState extends State<TelaReservas> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: _home(),
+      body: homeReservas(),
     );
   }
 }
 
-_home() {
+homeReservas() {
   return Scaffold(
-    appBar: shareon_appbar(),
-    drawer: DrawerList(),
     body: Container(
       child: Column(
         children: <Widget>[
