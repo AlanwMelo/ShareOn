37f6df2a9cf03c44abbd42a8141159628c3da469
Alan Willian Melo <amelo967@gmail.com>
2019-10-08 17:20:27 -0300

    Alan

--- lib/telas/tela_historico.dart
+++ lib/telas/tela_historico.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:shareon/layout_listas/lista_historico_builder.dart';
-import 'package:shareon/utils/drawer_builder.dart';
-import 'package:shareon/utils/shareon_appbar.dart';
 import 'package:shareon/utils/texts.dart';
 
 class TelaHistorico extends StatefulWidget {
@@ -14,15 +12,13 @@ class _TelaHistoricoState extends State<TelaHistorico> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: _home(),
+      body: homeHistorico(),
     );
   }
 }
 
-_home() {
+homeHistorico() {
   return Scaffold(
-    appBar: shareon_appbar(),
-    drawer: DrawerList(),
     body: Container(
       child: Column(
         children: <Widget>[
