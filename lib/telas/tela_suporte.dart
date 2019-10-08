fb19e047a7b71f686b88ba154baa8f2c81e9a995
Alan Willian Melo <amelo967@gmail.com>
2019-10-08 17:19:18 -0300

    Alan

--- lib/telas/tela_suporte.dart
+++ lib/telas/tela_suporte.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:shareon/utils/drawer_builder.dart';
-import 'package:shareon/utils/shareon_appbar.dart';
 
 class TelaSuporte extends StatefulWidget {
   @override
@@ -12,15 +10,13 @@ class _TelaSuporteState extends State<TelaSuporte> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: _home(),
+      body: homeSuporte(),
     );
   }
 }
 
-_home() {
+homeSuporte() {
   return Scaffold(
-    appBar: shareon_appbar(),
-    drawer: DrawerList(),
     body: Container(
       child: Center(
         child: Text("Suporte"),
